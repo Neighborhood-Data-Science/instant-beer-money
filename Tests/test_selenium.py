2ad142c7333ac3ddb01d67c5d20b53a7fe6a2c9c
Brandon Savone <38044300+DaMaddCyantist@users.noreply.github.com>
2022-01-25 12:08:26 -0600

    test

--- Tests/test_selenium.py
+++ Tests/test_selenium.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import timeit
 
