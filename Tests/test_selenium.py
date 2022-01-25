b862c90e4a4c9656bed57a9ca865bb9d3515744a
Brandon Savone <38044300+DaMaddCyantist@users.noreply.github.com>
2022-01-25 11:50:08 -0600

    More selnium tests

--- Tests/test_selenium.py
+++ Tests/test_selenium.py
@@ -5,11 +5,6 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import timeit
 
-# instantiate a chrome options object so you can set the size and headless preference
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
-
 @pytest.fixture(scope="class")
 def webdriver_values():
     ayet = "https://www.ayetstudios.com/offers/web_offerwall/2693?external_identifier=fsid-2308045-607783a635"
@@ -17,7 +12,7 @@ def webdriver_values():
 
 @pytest.fixture(scope="class")
 def setup_driver():
-    _driver = webdriver.Chrome(options=chrome_options,service_args=["--verbose", "--log-path=test-reports/chrome.log"])
+    _driver = webdriver.Chrome(service_args=["--verbose", "--log-path=test-reports/chrome.log"])
     yield _driver
     _driver.close()
 
