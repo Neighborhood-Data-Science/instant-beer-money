61ca090eebea320c1fe8360160a93cad8aa43a02
Brandon Savone <38044300+DaMaddCyantist@users.noreply.github.com>
2022-01-25 13:18:54 -0600

    Updating testing

--- Tests/test_selenium.py
+++ Tests/test_selenium.py
@@ -1,21 +1,41 @@
+"""
+Main Web Testing Framework
+"""
+
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import timeit
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
 
 @pytest.fixture(scope="class")
 def webdriver_values():
+    """
+    Test Doc Info
+    """
     ayet = "https://www.ayetstudios.com/offers/web_offerwall/2693?external_identifier=fsid-2308045-607783a635"
     yield ayet
 
 @pytest.fixture(scope="class")
 def setup_driver():
-    _driver = webdriver.Chrome()
+    """
+    Test Doc Info
+    """
+    _driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
     yield _driver
     _driver.close()
 
 def test_webdriver_circle(setup_driver,webdriver_values):
-    _driver.get(webdriver_values)
-    html=_driver.page_source
+    """
+    Test Doc Info
+    """
+    setup_driver.get(webdriver_values)
+    html= setup_driver.page_source
     soup=BeautifulSoup(html,'html.parser')
-    assert (len(soup.getText())>1)
+    assert len(soup.getText())>1
