3ebd400dcfb638e3cea23cc03bba7d2bfcf89ef0
DaMaddCyantist <38044300+DaMaddCyantist@users.noreply.github.com>
2022-01-28 04:44:36 -0600

    Files successfully pull data from correct offerwall Testing for CI/CD.

--- Functions/freecash_info.py
+++ Functions/freecash_info.py
@@ -3,15 +3,19 @@ This module will contain code necessary to connect to and
 scrape data from freecash.com
 """
 import os
+import time
+import pandas as pd
+import utils
 
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import pandas as pd
 
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #Set options to run Chrome in 'Headless' mode
 chrome_options = Options()
@@ -23,20 +27,27 @@ load_dotenv()
 #The environment variables should be present in your CI/CD pipeline
 #and/or server side as well.
 
-def start_driver_and_open_ayet():
+def start_driver_and_open_ayet(offerwall_version):
     """
     Downloads the latest version of Google chromedriver and
     Opens the Ayet offerwall within a webdriver
+
+    str: offerwall
+
+    offerwall examples:
+    - 'BASELINE_AYET'
+    - 'USER_AYET'
     """
     #This installs the latest version of the official Google chromedriver
     #Accesses cached version if present.
     try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),\
+                options=chrome_options)
     except Exception as err:
         print('Setting Driver Error: ' + err)
 
     #Set Ayet URL
-    ayet = os.environ['AYET']
+    ayet = os.environ[offerwall_version]
     #Open webpage
     driver.get(ayet)
     return driver
@@ -50,7 +61,8 @@ def parse_offer_information(driver):
     Also determines if Multiple Reward or not.
     """
     #Create dictionary to hold offer information
-    offer_dict = {'total_coins_earnable':[],'offer_title':[],'offer_description':[],'multiple_rewards':[]}
+    offer_dict = {'total_coins_earnable':[],'offer_title':[],\
+                  'offer_description':[],'multiple_rewards':[]}
     #Focus on offerwall information
     offer_info = driver.find_elements(By.XPATH, "//div[@class='row offer-row-basic offer-row-basic-cl ']")
     ############# LOGIC BLOCK ##############
@@ -88,3 +100,62 @@ def create_offer_dataframe(offer_dict):
     """
     offer_dataframe = pd.DataFrame.from_dict(offer_dict)
     return offer_dataframe
+
+def parse_completed_offer_info(driver):
+    """
+    This function parses through the Ayet offerwall text and extracts:
+    1. Completed Offer Titles
+    2. Completed Offer Date
+    3. Completed Offer Amount
+    ### Need Device ###
+    """
+    #Create dictionary to hold offer information
+    completed_offer_dict = {'offer_title':[],'coins_earned':[],'date_completed':[]}
+
+    #Navigate to reward status page
+    try:
+        #Wait for hamburger button to appear
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "hamburger-button")))
+        #Explicit Wait
+        time.sleep(1)
+        #Click button
+        driver.find_element(by=By.ID,value='hamburger-button').click()
+
+        #Wait for reward status button to appear
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "sidebar-status")))
+        #Explicit wait
+        time.sleep(1)
+        #Click button
+        driver.find_element(by=By.ID,value='sidebar-status').click()
+    except Exception as err:
+        print('Error: %s' % (err))
+
+    #Attempt to scrape - Fails if offers are not marked as 'Complete' yet
+    try:
+        #Wait for completed page to render
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "user-status")))
+        #Explicit wait
+        time.sleep(1)
+        #Extract completed offer info
+        completed_offer_info = driver.find_elements(By.XPATH, "//div[@id='user-status']")[0]
+
+        ############# LOGIC BLOCK ##############
+        #Get actual text from webpage
+        completed_offer_text = completed_offer_info.text
+        #Split the text
+        split_completed_offer_info = completed_offer_text.split('\n')
+        #Chunk the text for insert into dictionary
+        chunked_completed_list = utils.chunked_iterable(split_completed_offer_info, 4)
+        #Insert values to dictionary
+        for chunks in chunked_completed_list:
+            completed_offer_dict['offer_title'].append(chunks[1])
+            completed_offer_dict['coins_earned'].append(chunks[2])
+            completed_offer_dict['date_completed'].append(chunks[3])
+        ############# LOGIC BLOCK ##############
+
+        return completed_offer_dict
+    except Exception as err:
+        print('Unhandled Error: %s' % (err))
--- Tests/test_freecash.py
+++ Tests/test_freecash.py
@@ -9,38 +9,51 @@ import pandas as pd
 import pytest
 import os
 from dotenv import load_dotenv
+
 #Load environment variables
 load_dotenv()
 #The environment variables should be present in your CI/CD pipeline
 #and/or server side as well.
 
 @pytest.fixture(scope="class",autouse=True)
-def setup_ayet_page(request):
+def setup_raw_ayet_page(request):
     """
-    Set Ayet page as fixture
+    Set Ayet page as fixture using baseline ayet offerwall
     """
-    ayet_page = freecash_info.start_driver_and_open_ayet()
-    request.cls.ayet_page = ayet_page
-    yield ayet_page
-    ayet_page.close()
+    offerwall_version = 'MASTER_AYET'
+    ayet_base_page = freecash_info.start_driver_and_open_ayet(offerwall_version)
+    request.cls.ayet_base_page = ayet_base_page
+    yield ayet_base_page
+    ayet_base_page.quit()
 
-@pytest.mark.usefixtures("setup_ayet_page")
-class TestAYET:
+@pytest.fixture(scope="class",autouse=True)
+def setup_main_user_ayet_page(request):
     """
-    Full Ayet test suite
+    Set Ayet page as fixture using main user ayet offerwall
+    """
+    offerwall_version = 'AYET'
+    ayet_main_page = freecash_info.start_driver_and_open_ayet(offerwall_version)
+    request.cls.ayet_main_page = ayet_main_page
+    yield ayet_main_page
+    ayet_main_page.quit()
+
+@pytest.mark.usefixtures("setup_raw_ayet_page")
+class TestBaselineAYET:
+    """
+    Baseline Ayet Offerwall test suite
     """
     def test_access_ayet_offerwall(self):
         """
         Tests if able to successfully open Ayet Offerwall
         """
-        full_ayet_page = self.ayet_page
-        assert full_ayet_page.current_url == os.environ['AYET']
+        full_ayet_page = self.ayet_base_page
+        assert full_ayet_page.current_url == os.environ['MASTER_AYET']
 
     def test_parsed_offer_info_is_dict(self):
         """
         Tests if output of function is an expected dictionary.
         """
-        full_ayet_page = self.ayet_page
+        full_ayet_page = self.ayet_base_page
         offer_dict = freecash_info.parse_offer_information(full_ayet_page)
         assert isinstance(offer_dict, dict)
 
@@ -49,7 +62,7 @@ class TestAYET:
         Tests if length of key:value pairs in offer dict are
         the same size across all key:value lists.
         """
-        full_ayet_page = self.ayet_page
+        full_ayet_page = self.ayet_base_page
         offer_dict = freecash_info.parse_offer_information(full_ayet_page)
         key_list = list(offer_dict.keys())
         assert len(offer_dict.get(key_list[0])) == \
@@ -60,7 +73,7 @@ class TestAYET:
         Tests if resulting offer dictionary is the correct size.
         Should have length of (4) [keys].
         """
-        full_ayet_page = self.ayet_page
+        full_ayet_page = self.ayet_base_page
         offer_dict = freecash_info.parse_offer_information(full_ayet_page)
         assert len(offer_dict) == size
 
@@ -68,7 +81,7 @@ class TestAYET:
         """
         Tests if resultant dataframe is type dataframe.
         """
-        full_ayet_page = self.ayet_page
+        full_ayet_page = self.ayet_base_page
         offer_dict = freecash_info.parse_offer_information(full_ayet_page)
         offer_dataframe = pd.DataFrame(offer_dict)
         assert isinstance(offer_dataframe, pd.DataFrame)
@@ -77,9 +90,43 @@ class TestAYET:
         """
         Tests if all values in multiple_rewards column are either 1 or 0
         """
-        full_ayet_page = self.ayet_page
+        full_ayet_page = self.ayet_base_page
         offer_dict = freecash_info.parse_offer_information(full_ayet_page)
         offer_dataframe = pd.DataFrame(offer_dict)
         indicator_values = offer_dataframe['multiple_rewards'].unique()
         assert len(indicator_values) == 2
         assert [0 and 1] in indicator_values
+
+@pytest.mark.usefixtures("setup_main_user_ayet_page")
+class TestMainUserAYET:
+    """
+    Main User Ayet Offerwall test suite
+    """
+    def test_parsed_completed_offer_info_is_dict(self):
+        """
+        Tests if output of function is an expected dictionary.
+        """
+        full_ayet_page = self.ayet_main_page
+        completed_offer_dict = freecash_info.parse_completed_offer_info(full_ayet_page)
+        assert isinstance(completed_offer_dict, dict)
+
+    def test_parsed_completed_offer_info_dict_size(self,size=3):
+        """
+        Tests if resulting completed offer dictionary is the correct size.
+        Should have length of (3) [keys].
+        """
+        full_ayet_page = self.ayet_main_page
+        completed_offer_dict = freecash_info.parse_completed_offer_info(full_ayet_page)
+        assert len(completed_offer_dict) == size
+
+    def test_parsed_completed_offer_info_is_equal(self):
+        """
+        Tests if length of key:value pairs in comnpleted offer dict are
+        the same size across all key:value lists.
+        """
+        full_ayet_page = self.ayet_main_page
+        completed_offer_dict = freecash_info.parse_completed_offer_info(full_ayet_page)
+        key_list = list(completed_offer_dict.keys())
+        assert len(completed_offer_dict.get(key_list[0])) == \
+                len(completed_offer_dict.get(key_list[1])) == \
+                len(completed_offer_dict.get(key_list[2]))
