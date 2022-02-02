f793997e99a981ea9c35b3fde0c9951a911f73e3
Brandon Savone <38044300+DaMaddCyantist@users.noreply.github.com>
2022-02-01 23:27:06 -0600

    Updated conftest and moved adgem_info.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -7,7 +7,7 @@ any test across the application.
 import pytest
 import sys
 sys.path.append('./Functions')
-import freecash_info
+import ayet_info
 
 @pytest.fixture(scope="class",autouse=True)
 def setup_raw_ayet_page(request):
@@ -15,7 +15,7 @@ def setup_raw_ayet_page(request):
     Set Ayet page as fixture using baseline ayet offerwall
     """
     offerwall_version = 'MASTER_AYET'
-    ayet_base_page = freecash_info.start_driver_and_open_ayet(offerwall_version)
+    ayet_base_page = ayet_info.start_driver_and_open_ayet(offerwall_version)
     ayet_base_page.maximize_window()
     yield ayet_base_page
     ayet_base_page.close()
@@ -26,7 +26,7 @@ def setup_main_user_ayet_page(request):
     Set Ayet page as fixture using main user ayet offerwall
     """
     offerwall_version = 'AYET'
-    ayet_main_page = freecash_info.start_driver_and_open_ayet(offerwall_version)
+    ayet_main_page = ayet_info.start_driver_and_open_ayet(offerwall_version)
     ayet_main_page.maximize_window()
     yield ayet_main_page
     ayet_main_page.close()
