9369dd9c1b5351065688606f6087512a485ff61d
Brandon Savone <38044300+DaMaddCyantist@users.noreply.github.com>
2023-04-10 16:02:02 -0500

    F/Get info from revenue universe (#72)
    * Initial upload for RevU
    
    * Update config.yml
    
    Update to latest version for Python and Browser tools for CircleCI config.
    
    * Update requirements file
    
    * Remove unneeed file
    
    * Updated RevU functions and others
    Updating codebase.
    RevU works as expected to retrieve the offers and amounts.
    Rewrote some tests and conftest to simplify test suite.
    
    Will make new branch to focus on updating entire test suite.
    Afterwards, will turn CI/CD back on (CircleCI)
    
    * Minor update to .gitignore
    
    * Update to an import statement
    
    * Updated for coding standards

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -7,25 +7,15 @@ any test across the application.
 import pytest
 import sys
 sys.path.append('./Functions')
-import ayet_info
-import adgem_info
-import offertoro_info
+import ayet_info # type: ignore # Pylance(reportMissingImports)
+import adgem_info # type: ignore # Pylance(reportMissingImports)
+import offertoro_info # type: ignore # Pylance(reportMissingImports)
+import revu_info # type: ignore # Pylance(reportMissingImports)
 
 @pytest.fixture(scope="class",autouse=False)
-def setup_raw_ayet_page(request):
+def setup_ayet_page(request):
     """
-    Set Ayet page as fixture using baseline ayet offerwall
-    """
-    offerwall_version = 'MASTER_AYET'
-    ayet_base_page = ayet_info.start_driver_and_open_ayet(offerwall_version)
-    ayet_base_page.maximize_window()
-    yield ayet_base_page
-    ayet_base_page.close()
-
-@pytest.fixture(scope="class",autouse=False)
-def setup_main_user_ayet_page(request):
-    """
-    Set Ayet page as fixture using main user adgem offerwall
+    Set Ayet page as fixture. 
     """
     offerwall_version = 'AYET'
     ayet_main_page = ayet_info.start_driver_and_open_ayet(offerwall_version)
@@ -34,20 +24,9 @@ def setup_main_user_ayet_page(request):
     ayet_main_page.close()
 
 @pytest.fixture(scope="class",autouse=False)
-def setup_raw_adgem_page(request):
+def setup_adgem_page(request):
     """
-    Set Adgem page as fixture using baseline adgem offerwall
-    """
-    offerwall_version = 'MASTER_ADGEM'
-    adgem_base_page = adgem_info.start_driver_and_open_adgem(offerwall_version)
-    adgem_base_page.maximize_window()
-    yield adgem_base_page
-    adgem_base_page.close()
-
-@pytest.fixture(scope="class",autouse=False)
-def setup_main_user_adgem_page(request):
-    """
-    Set Adgem page as fixture using main user adgem offerwall
+    Set Adgem page as fixture. 
     """
     offerwall_version = 'ADGEM'
     adgem_main_page = adgem_info.start_driver_and_open_adgem(offerwall_version)
@@ -56,23 +35,23 @@ def setup_main_user_adgem_page(request):
     adgem_main_page.close()
 
 @pytest.fixture(scope="class",autouse=False)
-def setup_raw_toro_page(request):
+def setup_toro_page(request):
     """
-    Set OfferToro page as fixture using baseline Offertoro offerwall
-    """
-    offerwall_version = 'MASTER_TORO'
-    toro_base_page = offertoro_info.start_driver_and_open_offertoro(offerwall_version)
-    toro_base_page.maximize_window()
-    yield toro_base_page
-    toro_base_page.close()
-
-@pytest.fixture(scope="class",autouse=False)
-def setup_main_user_toro_page(request):
-    """
-    Set OfferToro page as fixture using main user OfferToro offerwall
+    Set OfferToro page as fixture.
     """
     offerwall_version = 'TORO'
     toro_main_page = offertoro_info.start_driver_and_open_offertoro(offerwall_version)
     toro_main_page.maximize_window()
     yield toro_main_page
     toro_main_page.close()
+
+@pytest.fixture(scope="class",autouse=False)
+def setup_revu_page(request):
+    """
+    Set Revenue Univerise page as fixture.
+    """
+    offerwall_version = 'REVU'
+    revu_main_page = revu_info.start_driver_and_open_revu(offerwall_version)
+    revu_main_page.maximize_window()
+    yield revu_main_page
+    revu_main_page.close()
---
+++ Tests/test_revu.py
@@ -0,0 +1,66 @@
+"""
+Main Web Testing Framework
+"""
+import sys
+sys.path.append('./Functions')
+
+import revu_info # type: ignore # Pylance(reportMissingImports)
+import pandas as pd
+import pytest
+import os
+from dotenv import load_dotenv
+
+#Load environment variables
+load_dotenv()
+#The environment variables should be present in your CI/CD pipeline
+#and/or server side as well.
+
+@pytest.mark.usefixtures("setup_revu_page")
+class TestREVU:
+    """
+    Test suite for the Revenue Universe offerwall.
+
+    This test suite is designed to test the functionality of `revu_info.py` to ensure 
+    it operates as expected.
+    """
+    def test_access_revu_offerwall(self,setup_revu_page):
+        """
+        Tests if able to successfully open RevU offerwall
+        """
+        assert setup_revu_page.current_url == os.environ['REVU']
+
+
+    def test_parsed_offer_info_is_dict(self,setup_revu_page):
+        """
+        Tests if output of function is an expected dictionary.
+        """
+        offer_dict = revu_info.parse_offer_information(setup_revu_page)
+        assert isinstance(offer_dict, dict)
+
+
+    def test_parsed_offer_info_is_equal(self,setup_revu_page):
+        """
+        Tests if length of key:value pairs in offer dict are
+        the same size across all key:value lists.
+        """
+        offer_dict = revu_info.parse_offer_information(setup_revu_page)
+        key_list = list(offer_dict.keys())
+        assert len(offer_dict.get(key_list[0])) == len(offer_dict.get(key_list[1]))
+
+
+    def test_parsed_offer_info_dict_size(self,setup_revu_page,size=2):
+        """
+        Tests if resulting offer dictionary is the correct size.
+        Should have length of 2 keys.
+        """
+        offer_dict = revu_info.parse_offer_information(setup_revu_page)
+        assert len(offer_dict) == size
+
+
+    def test_parsed_dataframe_is_dataframe(self,setup_revu_page):
+        """
+        Tests if resultant dataframe is type dataframe.
+        """
+        offer_dict = revu_info.parse_offer_information(setup_revu_page)
+        offer_dataframe = revu_info.create_offer_dataframe(offer_dict)
+        assert isinstance(offer_dataframe, pd.DataFrame)
