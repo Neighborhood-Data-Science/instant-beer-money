9a446af178c1384e2c0003e94923b9570119b65e
Brandon Savone <38044300+DaMaddCyantist@users.noreply.github.com>
2023-05-27 19:00:00 -0500

    Merge Main (#81)
    * F/update test suite (#76)
    
    * Update Test Case language
    Cleaned up some test case language.
    Submitting to see how CircleCI responds.
    
    * Added test case REVU
    Added additional test case to ensure data is actually being pulled
    
    * Restructured Adgem
    Added additional test case.
    Modernized code-base for easier readability.
    
    Two test cases fail due to inability to access URL in headless mode.
    Will try to fix for headless, but may switch to non-headless for access.
    
    * Revise Test case
    Created more general test case for verifying dictionary size.
    Takes an arbitrary list of key:value pairs vs. hard-coded
    
    * Add Ayet
    Ayet offerwall complete.
    Data is raw, and requires more cleaning, but we have what we need to start this process
    
    * Adding sys.exit(0) to adgem
    
    * Fix library import
    
    * Pushing adgem w/o headless mode
    
    * Added new test cases for Adgem
    
    * Updated config.yml
    
    * Updated config.yml
    
    * WORKING: Push for backup
    
    * Add correct error message verbage
    Errors message for functions were incorrect.
    
    * Updated Toro and Toro test cases
    
    * Update text case titles
    Made it easier to identify which offerwall is being tested
    
    * Update CircleCI python version to 3.11
    
    * Re-add headless mode for Adgem.
    Tests should still break.
    
    * Remove utils and utils tests
    Not needed any longer.
    
    * Remove utils from functions
    
    * Update to account for new environment name
    
    * Added 'new' parameter to headless.
    This should bypass error from Adgem
    
    * Added xFail for adgem
    
    * F/add idle furry (#77)
    
    * Add files via upload
    
    * Delete idlefurry.zip
    
    Need to reupload. 
    On mobile so have to delete first
    
    * Add files via upload
    
    New version. Includes `Forced Ad` scenario
    
    * Add README for Idle Furry
    
    * Move to correct folder
    
    * Delete idlefurry.zip
    
    * Add files via upload
    
    * Bump requests from 2.28.2 to 2.31.0 in /Tests (#78)
    
    ---------
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- Tests/test_offertoro.py
+++ Tests/test_offertoro.py
@@ -15,117 +15,60 @@ load_dotenv()
 #The environment variables should be present in your CI/CD pipeline
 #and/or server side as well.
 
-@pytest.mark.usefixtures("setup_raw_toro_page")
-class TestBaselineTORO:
+@pytest.mark.usefixtures("setup_toro_page")
+class TestTORO:
     """
-    Baseline OfferToro Offerwall test suite
+    Test suite for the Offertoro offerwall.
+
+    This test suite is designed to test the functionality of `offeroffertoro_info.py` to ensure 
+    it operates as expected.
     """
-    def test_access_toro_offerwall(self,setup_raw_toro_page):
+    def test_access_toro_offerwall(self,setup_toro_page):
         """
         Tests if able to successfully open OfferToro Offerwall
         """
-        assert setup_raw_toro_page.current_url == os.environ['MASTER_TORO']
+        assert setup_toro_page.current_url == os.environ['TORO']
 
-
-    def test_parsed_offer_info_is_dict(self,setup_raw_toro_page):
+    def test_parsed_offer_info_is_dict_toro(self,setup_toro_page):
         """
         Tests if output of function is an expected dictionary.
         """
-        offer_dict = offertoro_info.parse_available_offer_information(setup_raw_toro_page)
+        offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
         assert isinstance(offer_dict, dict)
 
-
-    def test_parsed_offer_info_is_equal(self,setup_raw_toro_page):
+    def test_parsed_offer_info_is_equal_toro(self,setup_toro_page):
         """
         Tests if length of key:value pairs in offer dict are
         the same size across all key:value lists.
         """
-        offer_dict = offertoro_info.parse_available_offer_information(setup_raw_toro_page)
+        offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
         key_list = list(offer_dict.keys())
-        assert len(offer_dict.get(key_list[0])) ==  \
-            len(offer_dict.get(key_list[1])) ==     \
-            len(offer_dict.get(key_list[2])) ==     \
-            len(offer_dict.get(key_list[3]))
+        first_size = len(offer_dict[key_list[0]])
+        for remaining_keys in key_list[1:]:
+            assert len(offer_dict[remaining_keys]) == first_size
 
 
-    def test_parsed_offer_info_dict_size(self,setup_raw_toro_page,size=4):
+    def test_parsed_offer_info_dict_size_toro(self,setup_toro_page,size=3):
         """
         Tests if resulting offer dictionary is the correct size.
-        Should have length of (4) [keys].
+        Should have length of 3 keys.
         """
-        offer_dict = offertoro_info.parse_available_offer_information(setup_raw_toro_page)
+        offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
         assert len(offer_dict) == size
 
 
-    def test_parsed_dataframe_is_dataframe(self,setup_raw_toro_page):
+    def test_parsed_dataframe_is_dataframe_toro(self,setup_toro_page):
         """
         Tests if resultant dataframe is type dataframe.
         """
-        offer_dict = offertoro_info.parse_available_offer_information(setup_raw_toro_page)
-        offer_dataframe = offertoro_info.create_available_offer_dataframe(offer_dict)
+        offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
+        offer_dataframe = offertoro_info.create_offer_dataframe(offer_dict)
         assert isinstance(offer_dataframe, pd.DataFrame)
-
-    def test_values_in_devices(self,setup_raw_toro_page):
+    
+    def test_parsed_dataframe_not_empty_toro(self, setup_toro_page):
         """
-        Tests if all values in offer_device column are either:
-        device
-        android phone
-        iphone/ipad
-        iphone
-        android tablet
+        Tests if resultant dataframe is not empty.
         """
-        device_list = ['device','android phone','iphone/ipad','iphone','android tablet']
-        offer_dict = offertoro_info.parse_available_offer_information(setup_raw_toro_page)
-        offer_dataframe = offertoro_info.create_available_offer_dataframe(offer_dict)
-        offer_device_vals = offer_dataframe['offer_device'].unique()
-        assert all(devices in offer_device_vals for devices in device_list)
-
-
-@pytest.mark.usefixtures("setup_main_user_toro_page")
-class TestMainUserTORO:
-    """
-    Main User OfferToro Offerwall test suite
-    """
-    def test_access_offerwall_main_user(self,setup_main_user_toro_page):
-        """
-        Tests if able to successfully open OfferToro Offerwall
-        """
-        assert setup_main_user_toro_page.current_url == os.environ['TORO']
-
-    def test_parsed_completed_offer_info_is_dict(self,setup_main_user_toro_page):
-        """
-        Tests if output of function is an expected dictionary.
-        """
-        completed_offer_dict = offertoro_info.parse_completed_offer_info(setup_main_user_toro_page)
-        assert isinstance(completed_offer_dict, dict)
-
-
-    def test_parsed_completed_offer_info_dict_size(self, setup_main_user_toro_page, size=4):
-        """
-        Tests if resulting completed offer dictionary is the correct size.
-        Should have length of (4) [keys].
-        """
-        completed_offer_dict = offertoro_info.parse_completed_offer_info(setup_main_user_toro_page)
-        assert len(completed_offer_dict) == size
-
-
-    def test_parsed_completed_offer_info_is_equal(self, setup_main_user_toro_page):
-        """
-        Tests if length of key:value pairs in comnpleted offer dict are
-        the same size across all key:value lists.
-        """
-        completed_offer_dict = offertoro_info.parse_completed_offer_info(setup_main_user_toro_page)
-        key_list = list(completed_offer_dict.keys())
-        assert len(completed_offer_dict.get(key_list[0])) == \
-                len(completed_offer_dict.get(key_list[1])) == \
-                len(completed_offer_dict.get(key_list[2])) == \
-                len(completed_offer_dict.get(key_list[3]))
-
-
-    def test_completed_dataframe_is_dataframe(self, setup_main_user_toro_page):
-        """
-        Tests if resultant dataframe is type dataframe.
-        """
-        completed_offer_dict = offertoro_info.parse_completed_offer_info(setup_main_user_toro_page)
-        completed_offer_dataframe = offertoro_info.create_completed_offer_dataframe(completed_offer_dict)
-        assert isinstance(completed_offer_dataframe, pd.DataFrame)
+        offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
+        offer_dataframe = offertoro_info.create_offer_dataframe(offer_dict)
+        assert len(offer_dataframe) > 0
