ca507b9bfff39f6d7362f9fa18bc4c1cd30f23b7
Brandon Savone <brandonsavone@gmail.com>
2023-06-07 14:29:32 -0500

    Added info to connect to mySQL database

--- Main/main_db_push.py
+++ Main/main_db_push.py
@@ -5,16 +5,34 @@ Main Framework for pulling data from Freecash offerwalls and pushing it to the d
 import pandas as pd
 import pytest
 import os
+import mysql.connector
 from dotenv import load_dotenv
-from selenium.webdriver.common.by import By
+from mysql.connector import Error
 import sys
 sys.path.append('./Functions')
 import adgem_info
 import ayet_info
 import offertoro_info
 import revu_info
+import offer_cleanup
 
 #Load environment variables
 load_dotenv()
 #The environment variables should be present in your CI/CD pipeline
 #and/or server side as well.
+
+
+try:
+    connection = mysql.connector.connect(host=os.environ['DB_HOST'],
+                                        user=os.environ['DB_USER'],
+                                        password=os.environ['DB_PASSWORD'])
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed")
