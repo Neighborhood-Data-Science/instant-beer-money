c9284feb313e29ad46bd16ce71b3b7d4843a1f32
Brandon Savone <brandonsavone@gmail.com>
2023-06-16 16:37:13 -0500

    Added additional tests for database testing. Also adjusted conftest to accomdate new tests. Many syntax fixes. Standardizing using Pep8 (pylint)

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,17 +4,16 @@ This file will hold all fixtures that are needed in
 any test across the application.
 
 """
+import os
 import pytest
-import pandas as pd # type: ignore # Pylance(reportMissingImports)
-import sys
-sys.path.append('./Functions')
-import ayet_info # type: ignore # Pylance(reportMissingImports)
-import adgem_info # type: ignore # Pylance(reportMissingImports)
-import offertoro_info # type: ignore # Pylance(reportMissingImports)
-import revu_info # type: ignore # Pylance(reportMissingImports)
-import offer_cleanup # type: ignore # Pylance(reportMissingImports)
+import pandas as pd
+import ayet_info
+import adgem_info
+import offertoro_info
+import revu_info
+import mysql.connector
 
-@pytest.fixture(scope="class",autouse=False)
+@pytest.fixture(scope="class", autouse=False)
 def setup_ayet_page(request):
     """
     Set Ayet page as fixture. 
@@ -25,7 +24,8 @@ def setup_ayet_page(request):
     yield ayet_main_page
     ayet_main_page.close()
 
-@pytest.fixture(scope="class",autouse=False)
+
+@pytest.fixture(scope="class", autouse=False)
 def setup_adgem_page(request):
     """
     Set Adgem page as fixture. 
@@ -36,18 +36,21 @@ def setup_adgem_page(request):
     yield adgem_main_page
     adgem_main_page.close()
 
-@pytest.fixture(scope="class",autouse=False)
+
+@pytest.fixture(scope="class", autouse=False)
 def setup_toro_page(request):
     """
     Set OfferToro page as fixture.
     """
     offerwall_version = 'TORO'
-    toro_main_page = offertoro_info.start_driver_and_open_offertoro(offerwall_version)
+    toro_main_page = offertoro_info.start_driver_and_open_offertoro(
+        offerwall_version)
     toro_main_page.maximize_window()
     yield toro_main_page
     toro_main_page.close()
 
-@pytest.fixture(scope="class",autouse=False)
+
+@pytest.fixture(scope="class", autouse=False)
 def setup_revu_page(request):
     """
     Set Revenue Univerise page as fixture.
@@ -58,62 +61,118 @@ def setup_revu_page(request):
     yield revu_main_page
     revu_main_page.close()
 
-@pytest.fixture(scope="function",autouse=False)
+
+@pytest.fixture(scope="function", autouse=False)
 def qa_ayet(request):
     """
     Set mock Ayet data as fixture. 
     """
     data = {
-    'offerLow': ['10', '20', '30'],
-    'offerHigh': [None, '50', '60'],
-    'offer_name': ['Offer 1', 'Offer 2', 'Offer 3'],
-    'offer_description': ['Multiple rewards', 'Description 2', 'Multiple rewards'],
-    'Additional': ['Additional 1', None, 'Additional 2'],
-    'Difficulty': ['Easy', None, 'Medium'],
-    'Ignore3': [None, None, 'Value']
+        'offerLow': ['10', '20', '30'],
+        'offerHigh': [None, '50', '60'],
+        'offer_name': ['Offer 1', 'Offer 2', 'Offer 3'],
+        'offer_description': ['Multiple rewards', 'Description 2', 'Multiple rewards'],
+        'Additional': ['Additional 1', None, 'Additional 2'],
+        'Difficulty': ['Easy', None, 'Medium'],
+        'Ignore3': [None, None, 'Value']
     }
 
     ayet_frame = pd.DataFrame(data)
     return ayet_frame
 
-@pytest.fixture(scope="function",autouse=False)
+
+@pytest.fixture(scope="function", autouse=False)
 def qa_adgem(request):
     """
     Set mock Adgem data as fixture. 
     """
     data = {
-    'offer_title': ['Hidden Objects', 'Video Poker Classic', 'Smash Party'],
-    'offer_description': ['Complete Level 31 to Earn!', 'Complete Multiple Goals to Earn!',
-                        'Complete Multiple Goals to Earn!'],
-    'offer_amount': ['683', 'Up To\n11,250', 'Up To\n13,247'],
-    'offer_device': ['Apple', 'Android', 'Android']
+        'offer_title': ['Hidden Objects', 'Video Poker Classic', 'Smash Party'],
+        'offer_description': ['Complete Level 31 to Earn!', 'Complete Multiple Goals to Earn!',
+                              'Complete Multiple Goals to Earn!'],
+        'offer_amount': ['683', 'Up To\n11,250', 'Up To\n13,247'],
+        'offer_device': ['Apple', 'Android', 'Android']
     }
 
     adgem_frame = pd.DataFrame(data)
     return adgem_frame
 
-@pytest.fixture(scope="function",autouse=False)
+
+@pytest.fixture(scope="function", autouse=False)
 def qa_revu(request):
     """
     Set mock Revenue Universe data as fixture. 
     """
     data = {
-    'offer_title': ['Title 1', 'Title 2', 'Title 3'],
-    'offer_amount': ['255', '8,250', '165']
+        'offer_title': ['Title 1', 'Title 2', 'Title 3'],
+        'offer_amount': ['255', '8,250', '165']
     }
 
     revu_frame = pd.DataFrame(data)
     return revu_frame
 
-@pytest.fixture(scope="function",autouse=False)
+
+@pytest.fixture(scope="function", autouse=False)
 def qa_toro(request):
     """
     Set mock Offertoro data as fixture. 
     """
     data = {
-    'offer_title': ['\nTitle 1 - Source\nDescription 1', '\nTitle 2 - Source\nDescription 2'],
-    'offer_device': ['android phone', 'iphone/ipad']
+        'offer_title': ['\nTitle 1 - Source\nDescription 1', '\nTitle 2 - Source\nDescription 2'],
+        'offer_device': ['android phone', 'iphone/ipad']
     }
 
     toro_frame = pd.DataFrame(data)
     return toro_frame
+
+
+@pytest.fixture(scope="function", autouse=False)
+def main_db_mock(request):
+    """
+    Set mock dataframe data for testing main_db_push
+    """
+    data = {
+        'offer_title': ['Title 1', 'Title 2', 'Title 3'],
+        'offer_device': ['Device 1', 'Device 2', 'Device 3']
+    }
+
+    mock_frame = pd.DataFrame(data)
+    return mock_frame
+
+
+@pytest.fixture(scope="module", autouse=False)
+def db_connection():
+    """
+    Set connection to database for testing purposes
+    """
+    database_conn = mysql.connector.connect(
+        host=os.environ['DB_HOST'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        database=os.environ['DB_NAME']
+    )
+    yield database_conn
+    # Close connection after the test
+    database_conn.close()
+
+
+@pytest.fixture(scope="module", autouse=False)
+def db_table(db_connection):  # pylint: disable=W0621
+    """
+    Set up table for testing purposes
+    """
+    cursor = db_connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS TEST_TABLE (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL DEFAULT 'MASTER',
+            offer_title TEXT NOT NULL,
+            offer_device CHAR(25) DEFAULT 'MASTER'
+            )
+    """)
+    cursor.close()
+    yield
+    # Drop table after the test
+    cursor = db_connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS TEST_TABLE")
+    cursor.close()
--- Tests/test_database.py
+++ Tests/test_database.py
@@ -4,9 +4,10 @@ Main Testing Framework to confirm database connectivity and operation.
 
 import os
 import pytest
-import mysql.connector
-from dotenv import load_dotenv
+import main_db_push
 
+
+@pytest.mark.usefixtures("main_db_mock", "db_connection", "db_table")
 class TestDB:
     """
     Test suite for our mySQL Database.
@@ -19,8 +20,51 @@ class TestDB:
         """
         Test that we are able to connect to the database.
         """
-        connection = mysql.connector.connect(host=os.environ['DB_HOST'],
-                                            user=os.environ['DB_USER'],
-                                            password=os.environ['DB_PASSWORD'])
+        connection = main_db_push.establish_connection()
         assert connection.is_connected()
         connection.close()
+
+    def test_get_offerwall_data(self):
+        """
+        Test that we are able to retrieve data from the offerwalls
+        """
+        offer_data = main_db_push.get_offerwall_data()
+        assert isinstance(offer_data, list)
+        assert len(offer_data) > 0
+
+    def test_insert_data(self, main_db_mock, db_connection):
+        """
+        Test that we are able to insert data into the database.
+        """
+
+        # Create sample DataFrames for testing
+        dataframe_list = [main_db_mock, main_db_mock]
+
+        # Set cursor
+        cursor = db_connection.cursor()
+
+        for dataframe in dataframe_list:
+            # Collect the column names from the dataframe.
+            query_cols = ', '.join(dataframe.columns)
+            # Calculate the proper number of %s placeholders for each column in the dataframe.
+            value_len = ', '.join(['%s'] * len(dataframe.columns))
+            # Query to insert the data into the database
+            insert_query = f"INSERT INTO {os.environ['DB_TEST_TABLE']}\
+                 ({query_cols}) VALUES ({value_len})"
+            # Get the values from the DataFrame as a list of tuples
+            values = [tuple(row) for row in dataframe.values]
+            # Execute the INSERT statement with multiple rows
+            cursor.executemany(insert_query, values)
+            # Commit the changes to the database
+            db_connection.commit()
+
+        # Check validity of the database
+        cursor.execute("SHOW TABLES")
+        tables = [row[0] for row in cursor.fetchall()]
+        assert os.environ['DB_TEST_TABLE'] in tables
+
+        cursor.execute(f"SELECT * FROM {os.environ['DB_TEST_TABLE']}")
+        rows = cursor.fetchall()
+        assert len(rows) > 0
+        # Close the cursor
+        cursor.close()
