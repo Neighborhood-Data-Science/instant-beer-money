3b2df352ff44d828d2616ecb18a6335dd4c246c7
Brandon Savone <brandonsavone@gmail.com>
2023-06-18 11:28:23 -0500

    Cleanup offertoro files. We will see if tests actually execute.

--- Tests/test_offertoro.py
+++ Tests/test_offertoro.py
@@ -1,19 +1,17 @@
 """
 Main Web Testing Framework
 """
-import sys
-sys.path.append('./Functions')
-
+import os
 import offertoro_info
 import pandas as pd
 import pytest
-import os
 from dotenv import load_dotenv
 
-#Load environment variables
+# Load environment variables
 load_dotenv()
-#The environment variables should be present in your CI/CD pipeline
-#and/or server side as well.
+# The environment variables should be present in your CI/CD pipeline
+# and/or server side as well.
+
 
 @pytest.mark.usefixtures("setup_toro_page")
 class TestTORO:
@@ -23,20 +21,21 @@ class TestTORO:
     This test suite is designed to test the functionality of `offeroffertoro_info.py` to ensure 
     it operates as expected.
     """
-    def test_access_toro_offerwall(self,setup_toro_page):
+
+    def test_access_toro_offerwall(self, setup_toro_page):
         """
         Tests if able to successfully open OfferToro Offerwall
         """
         assert setup_toro_page.current_url == os.environ['TORO']
 
-    def test_parsed_offer_info_is_dict_toro(self,setup_toro_page):
+    def test_parsed_offer_info_is_dict_toro(self, setup_toro_page):
         """
         Tests if output of function is an expected dictionary.
         """
         offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
         assert isinstance(offer_dict, dict)
 
-    def test_parsed_offer_info_is_equal_toro(self,setup_toro_page):
+    def test_parsed_offer_info_is_equal_toro(self, setup_toro_page):
         """
         Tests if length of key:value pairs in offer dict are
         the same size across all key:value lists.
@@ -47,8 +46,7 @@ class TestTORO:
         for remaining_keys in key_list[1:]:
             assert len(offer_dict[remaining_keys]) == first_size
 
-
-    def test_parsed_offer_info_dict_size_toro(self,setup_toro_page,size=3):
+    def test_parsed_offer_info_dict_size_toro(self, setup_toro_page, size=3):
         """
         Tests if resulting offer dictionary is the correct size.
         Should have length of 3 keys.
@@ -56,15 +54,14 @@ class TestTORO:
         offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
         assert len(offer_dict) == size
 
-
-    def test_parsed_dataframe_is_dataframe_toro(self,setup_toro_page):
+    def test_parsed_dataframe_is_dataframe_toro(self, setup_toro_page):
         """
         Tests if resultant dataframe is type dataframe.
         """
         offer_dict = offertoro_info.parse_offer_information(setup_toro_page)
         offer_dataframe = offertoro_info.create_offer_dataframe(offer_dict)
         assert isinstance(offer_dataframe, pd.DataFrame)
-    
+
     def test_parsed_dataframe_not_empty_toro(self, setup_toro_page):
         """
         Tests if resultant dataframe is not empty.
