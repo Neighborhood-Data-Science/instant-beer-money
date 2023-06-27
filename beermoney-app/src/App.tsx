490dab126fbf31536f705998f34ac1a20ba3672a
Brandon Savone <brandonsavone@gmail.com>
2023-06-27 16:43:52 -0500

    Added test button for API call

--- beermoney-app/src/App.tsx
+++ beermoney-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ApiButton from './components/testButton.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <ApiButton />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
