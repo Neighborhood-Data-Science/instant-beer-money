ba80926137c3f6ab19ec2576126f2014b8b535f6
Brandon Savone <brandonsavone@gmail.com>
2023-06-27 16:43:40 -0500

    Added components folder Also added testbutton to verify expected behavior

---
+++ beermoney-app/src/components/testButton.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const ApiButton: React.FC = () => {
+    const [apiResponse, setApiResponse] = useState<any>('');
+  const handleClick = () => {
+    fetch('https://us-south.functions.appdomain.cloud/api/v1/web/a50fddb3-8a2d-40f0-83b3-862d10500265/src/simple_test', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // Add any necessary request body or parameters
+    //   body: JSON.stringify({"name":""}),
+    })
+      .then(response => response.text())
+      .then(data => {
+        // Update state with API response
+        setApiResponse(data);
+      })
+      .catch(error => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+        <button onClick={handleClick}>Click Me</button>
+        <div>{apiResponse}</div>
+    </div>
+  );
+};
+
+export default ApiButton;
