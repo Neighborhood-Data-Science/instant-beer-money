45ebf5331ca2b488117dcdfa6159011ec201f1dc
Brandon Savone <brandonsavone@gmail.com>
2023-06-28 15:07:04 -0500

    Readded baseURL as constant variable Tried to use environment variables to hide baseURL, but it seems redundant. Base url is simply an address to the API. API limits are in place to mitigate abuse.

--- beermoney-app/src/components/testButton_PROD.tsx
+++ beermoney-app/src/components/testButton_PROD.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 
+const baseURL = 'https://us-south.functions.appdomain.cloud/api/v1/web/a50fddb3-8a2d-40f0-83b3-862d10500265/'
+
 const ApiButton: React.FC = () => {
     const [apiResponse, setApiResponse] = useState<any>('');
   const handleClick = () => {
-    fetch('${source}src/simple_test', {
+    fetch(baseURL+'src/simple_test', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
