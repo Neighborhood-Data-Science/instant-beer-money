00b6fc2e9c45bfb65066022ba79cf1e058d72a19
Brandon Savone <brandonsavone@gmail.com>
2023-07-11 16:42:04 -0500

    Added capability to read info from DB and display

--- beermoney-app/src/components/showTable_button.tsx
+++ beermoney-app/src/components/showTable_button.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 
-const baseURL = 'https://c752tdu4nsjocjzhjzz4hxorze0cdyuv.lambda-url.us-east-2.on.aws/'
+const baseURL = 'https://c752tdu4nsjocjzhjzz4hxorze0cdyuv.lambda-url.us-east-2.on.aws/';
 
 const ShowButton: React.FC = () => {
-    const [apiResponse, setApiResponse] = useState<any>('');
-    const [isLoading, setIsLoading] = useState(false);
+  const [tableData, setTableData] = useState<any[]>([]); // Provide an initial empty array as default value
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleClick = () => {
     // Set state to true before making API call
     setIsLoading(true);
+
     fetch(baseURL, {
       method: 'GET',
       headers: {},
-      // Add any necessary request body or parameters
-    //   body: JSON.stringify({"name":""}),
     })
-      .then(response => response.text())
+      .then(response => response.json())
       .then(data => {
-        // Update state with API response
-        setApiResponse(data);
+        // Parse the JSON response
+        const parsedData = JSON.parse(data.body);
+        // Update state with parsed data
+        setTableData(parsedData);
       })
+      
       .catch(error => {
         // Handle any errors
         console.error(error);
@@ -27,14 +30,35 @@ const ShowButton: React.FC = () => {
         // Set state to false after API call is complete
         setIsLoading(false);
       });
-    };
+  };
 
   return (
     <div>
-        <button onClick={handleClick} disabled={isLoading}>
-          {isLoading ? 'Loading table' : 'Load Table from DB'}
-          </button>
-        <div>{apiResponse}</div>
+      <button onClick={handleClick} disabled={isLoading}>
+        {isLoading ? 'Loading table...' : 'Load Table from DB'}
+      </button>
+      {tableData.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Description</th>
+              {/* Add more column headers as needed */}
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map(row => (
+              <tr key={row[0]}>
+                <td>{row[0]}</td>
+                <td>{row[2]}</td>
+                <td>{row[3]}</td>
+                {/* Map the remaining columns to <td> elements */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
