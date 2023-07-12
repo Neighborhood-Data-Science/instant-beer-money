7cac2a97d4d00b57860be9ca503aa8c65247f627
Brandon Savone <brandonsavone@gmail.com>
2023-07-12 17:14:44 -0500

    Adding files for new hideRow button Lambda

---
+++ prod/lambda/hide_row/main_lambda.py
@@ -0,0 +1,73 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+
+def establish_connection():
+    """
+    Function to establish a connection to the database.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+        database_conn: `mysql.connector.connection_cext.CMySQLConnection` 
+            A connection to the database.
+    """
+
+    try:
+        database_conn = mysql.connector.connect(
+            host=os.environ['DB_HOST'],
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASSWORD'],
+            database=os.environ['DB_NAME']
+        )
+        return database_conn
+    except Error as process_error:
+        print(process_error)
+
+
+def update_table(database):
+    """
+    Function to update the 'hidden' value for a particular row in the database.
+
+    Parameters
+    ----------
+        database: `mysql.connector.connection_cext.CMySQLConnection` 
+            A successfully connected database.
+
+    Returns
+    -------
+        table_dump: `JSON object` 
+            A json object containing the data from the database.
+    """
+
+    # Create a cursor object to execute SQL queries
+    cursor = database.cursor()
+
+    # Retrieve all information from the database
+    # This is to be changed in future versions
+    sql = f"""
+    UPDATE {os.environ['DB_MAIN_TABLE']}
+    SET hidden = 1 /
+    WHERE id = rowId;
+    """
+
+    # Execute the SQL query
+    cursor.execute(sql)
+    
+    # Close the cursor and the connection
+    cursor.close()
+    database.close()
+
+    return 'SUCCESS'
+
+
+def lambda_handler(event=None, context=None):
+    """
+    The main execution step of the AWS Lambda function.
+    """
+    database_conn = establish_connection()
+    table_action = update_table(database_conn)
+    return table_action
