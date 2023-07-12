f672b38a99799299887aa418bf4291769ce47522
Brandon Savone <brandonsavone@gmail.com>
2023-07-12 17:15:02 -0500

    Removed a button that was not needed anymore

--- beermoney-app/src/App.tsx
+++ beermoney-app/src/App.tsx
@@ -19,9 +19,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
