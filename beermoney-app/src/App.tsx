b19e2e486ce2d5b7eaf08121daf11847f174172c
Brandon Savone <brandonsavone@gmail.com>
2023-07-17 17:57:37 -0500

    Just removed some unneeded lines

--- beermoney-app/src/App.tsx
+++ beermoney-app/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,7 +5,6 @@ import ScrapeButton from './components/initalScrape_button.tsx'
 import ShowButton from './components/showTable_button'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
       <div>
@@ -20,14 +18,10 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <ScrapeButton />
         <ShowButton />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
