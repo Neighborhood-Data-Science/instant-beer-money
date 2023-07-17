d8404ca25dfbda914d33af7b809ce5c2cce55ec0
Brandon Savone <brandonsavone@gmail.com>
2023-07-17 17:57:24 -0500

    Adjusted this component Dont think this is needed anymore actually.

--- beermoney-app/src/components/hideRow_button.tsx
+++ beermoney-app/src/components/hideRow_button.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 interface HideButtonProps {
   rowId: number;
   onHide: (rowId: number) => void;
+  handleHideSelected: () => void; // Added handleHideSelected function
 }
 
-const HideButton: React.FC<HideButtonProps> = ({ rowId, onHide }) => {
+const HideButton: React.FC<HideButtonProps> = ({ rowId, onHide, handleHideSelected }) => {
   const handleHide = () => {
     onHide(rowId);
   };
 
   return (
-    <button onClick={handleHide}>Hide</button>
+    <>
+      <button onClick={handleHide}>Hide</button>
+      <button onClick={handleHideSelected}>Hide Selected</button> {/* Added button for hiding selected rows */}
+    </>
   );
 };
 
