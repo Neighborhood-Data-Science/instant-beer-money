cf5eb8ef19b518e2e2166768917df6ef8a2b46b7
Brandon Savone <brandonsavone@gmail.com>
2023-09-25 17:16:12 -0500

    Update button on webpage to expect FSID as input

--- beermoney-app/src/components/initalScrape_button.tsx
+++ beermoney-app/src/components/initalScrape_button.tsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
 const baseURL = 'https://jxpokkhf6xnfks3qnbsqmaagim0wunnh.lambda-url.us-east-2.on.aws/';
 
 const ScrapeButton: React.FC = () => {
-  const [apiResponse, setApiResponse] = useState<any>(''); // Fix the destructured state setter here
-  const [isLoading, setIsLoading] = useState(false);
-  const [isComplete, setIsComplete] = useState(false);
+  const [fsid, setFsid] = useState<string>('');
+  const [apiResponse, setApiResponse] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isComplete, setIsComplete] = useState<boolean>(false);
+
+  const handleFsidChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFsid(e.target.value);
+  };
 
   const handleClick = () => {
+    // Check if the FSID is empty before making the API request
+    if (fsid.trim() === '') {
+      alert('Please enter an FSID.'); // You can display an error message to the user
+      return; // Prevent the API request if FSID is empty
+    }
+
     setIsLoading(true);
-    fetch(baseURL, {
+
+    // Include the FSID as a query parameter in the Lambda URL
+    fetch(`${baseURL}?fsid=${fsid}`, {
       method: 'POST',
       headers: {},
     })
-      .then(response => response.text())
-      .then(data => {
+      .then((response) => response.text())
+      .then((data) => {
         setApiResponse(data);
-        console.log('API Reponse: ', apiResponse);
+        console.log('API Response: ', apiResponse);
         if (data === 'SUCCESS') {
           setIsComplete(true);
           setIsLoading(false);
@@ -25,20 +38,30 @@ const ScrapeButton: React.FC = () => {
           }, 60000);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         setIsLoading(false);
       });
   };
 
+  // Check if fsid is empty to disable the button
+  const isButtonDisabled = fsid.trim() === '' || isLoading || isComplete;
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Enter FSID"
+        value={fsid}
+        onChange={handleFsidChange}
+        disabled={isLoading || isComplete}
+      />
       <button
         onClick={handleClick}
-        disabled={isLoading || isComplete}
+        disabled={isButtonDisabled}
         className={isComplete ? 'complete-button' : ''}
       >
-        {isLoading ? 'Scraping...Please Wait.' : isComplete ? 'Complete' : 'Execute Scrape'}
+        {isLoading ? 'Thank you! Please wait..' : isComplete ? 'Complete' : 'Execute Scrape'}
       </button>
     </div>
   );
